6cc080769ddf3c538a3f6c0d9a4c37eb6dc1be7c
kunal-kataria <kunalkataria1512@gmail.com>
2023-05-02 18:23:51 +0530

    Initial

---
+++ backend/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+	MongoURI:
+		"mongodb+srv://kunalkataria1512:b0AQBnttb53vGxzy@cluster0.byxdbdj.mongodb.net/?retryWrites=true&w=majority",
+};
+
+// mongodb+srv://kunalkataria1512:b0AQBnttb53vGxzy@cluster0.byxdbdj.mongodb.net/?retryWrites=true&w=majority
